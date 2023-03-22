46decc2346b431eac0efbb2b19bd4039d9572b21
natelee025 <natalialee025@gmail.com>
2023-03-22 16:05:26 +0300

    auth tests parametrization

--- tests/auth_page.py
+++ tests/auth_page.py
@@ -1,21 +1,39 @@
 import pytest
+from data_json import *
+from data_xlsx import *
+
+locked_out_username = [locked_out_username, correct_password, locked_out]
+incorrect_username = [incorrect_username, correct_password, not_match]
+incorrect_password = [correct_username, incorrect_password,not_match]
+
+empty_username = ['', correct_password, empty_login]
+empty_password = [correct_username, '', empty_password]
+empty_fields = ['', '', empty_login]
+
+space_username = [' ', correct_password, not_match]
+space_password = [correct_username, ' ', not_match]
 
 
 def test_successful_login(auth):
-    auth.enter_user_name('standard_user')
-    auth.enter_password('secret_sauce')
+    auth.enter_user_name(correct_username)
+    auth.enter_password(correct_password)
     auth.log_in()
     auth.assert_url('https://www.saucedemo.com/inventory.html')
 
 
-blocked_username = ['locked_out_user', 'secret_sauce', 'Epic sadface: Sorry, this user has been locked out.']
-incorrect_username = ['not_user', 'secret_sauce',
-                      'Epic sadface: Username and password do not match any user in this service']
-
-
-@pytest.mark.parametrize('username, password, text', (blocked_username, incorrect_username),
+@pytest.mark.parametrize('username, password, text', (locked_out_username, incorrect_username),
                          ids=['blocked_username', 'incorrect_username'])
 def test_unsuccessful_login(auth, username, password, text):
+    auth.enter_user_name(username)
+    auth.enter_password(password)
+    auth.log_in()
+    auth.assert_text(text)
+
+
+@pytest.mark.parametrize('username, password, text', (empty_username, empty_password, empty_fields,
+                                                      space_username, space_password),
+                         ids=['empty_username', 'empty_password', 'empty_fields', 'space_username', 'space_password'])
+def test_empty_fields(auth, username, password, text):
     auth.enter_user_name(username)
     auth.enter_password(password)
     auth.log_in()
